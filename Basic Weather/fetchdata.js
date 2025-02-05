fa9afeecb9ec330577525c40057241dffe5189ed
Vatsala <sanwalvatsala@gmail.com>
2025-02-05 15:20:13 +0530

    upadted js

--- Basic Weather/fetchdata.js
+++ Basic Weather/fetchdata.js
@@ -1,9 +1,12 @@
-const API_key = "33ef18b727142847aabd5878f494cb43";
+const API_key = "c72ac8510bddc8ec1fc3543ec0565ac0";
 
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=kolkata&units=metric&appid=${API_key}`)
     .then((data) => data.json())  
     .then((jsonData) => {
-        console.log(jsonData);  
+        console.log(jsonData.name)
+        console.log(jsonData.main.temp)
+        console.log(jsonData.main.feels_like)
+        console.log(jsonData.weather[0].description)
     })
     .catch((error) => {
         console.error("Error fetching weather data:", error);
